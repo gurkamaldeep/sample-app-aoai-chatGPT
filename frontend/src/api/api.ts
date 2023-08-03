da520610762e59565379fc9a29ecece6b77c514b
Gurkamal <gurkamaldeep@live.com>
2023-08-03 11:33:56 -0500

    commenting

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,28 +1,29 @@
-/**import { UserInfo, ConversationRequest } from "./models";
+/**
+ * import { UserInfo, ConversationRequest } from "./models";
 
-export async function conversationApi(options: ConversationRequest, abortSignal: AbortSignal): Promise<Response> {
-    const response = await fetch("/conversation", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            messages: options.messages
-        }),
-        signal: abortSignal
-    });
+*export async function conversationApi(options: ConversationRequest, abortSignal: AbortSignal): Promise<Response> {
+*    const response = await fetch("/conversation", {
+*        method: "POST",
+*        headers: {
+*            "Content-Type": "application/json"
+*        },
+*        body: JSON.stringify({
+*            messages: options.messages
+*        }),
+*        signal: abortSignal
+*    });
+*
+*    return response;
+*}
 
-    return response;
-}
-
-export async function getUserInfo(): Promise<UserInfo[]> {
-    const response = await fetch('/.auth/me');
-    if (!response.ok) {
-        console.log("No identity provider found. Access to chat will be blocked.")
-        return [];
-    }
-
-    const payload = await response.json();
-    return payload;
-}
+*export async function getUserInfo(): Promise<UserInfo[]> {
+*    const response = await fetch('/.auth/me');
+*    if (!response.ok) {
+*        console.log("No identity provider found. Access to chat will be blocked.")
+*        return [];
+*    }
+*
+*    const payload = await response.json();
+*    return payload;
+*}
 */
